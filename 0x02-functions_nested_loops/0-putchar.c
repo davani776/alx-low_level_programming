a6f827844108deda920d622c83f99366563ca1d5
Dawit Kassahun <kassahundawit04@gmail.com>
2023-10-10 07:43:12 -0700

    this is putchar based task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+   *main - main function
+   * return(0) - returns 0 if the program is true 
+   */
+
+int main()
+{
+	putchar(_);
+	putchar(p);
+        putchar(p);
+        putchar(p);
+        putchar(p);
+	putchar(p);
+        putchar(p);
+        putchar(p);
+        putchar(p);
+
+	return (0)
+}
