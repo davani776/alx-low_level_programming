d3e449610c80912a10ada75512f68796172ad0ee
Dawit Kassahun <kassahundawit04@gmail.com>
2023-10-16 17:40:47 -0700

    I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *
- *
- *
+ * _strlen - function to find lengh of a string
+ * @string_len :- initialzie string
+ * return : return lengh of string
  *
  */
 int _strlen(char *s)
 {
 	int string_len = 0;
+
 	while (s[string_len])
 	{
 		string_len++;
