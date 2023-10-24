8aeeb29a9a33dc148b9d9d47fb4682262e64bc46
Dawit Kassahun <kassahundawit04@gmail.com>
2023-10-23 23:31:22 -0700

    upper case task

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,17 @@
 #include "main.h"
 /**
- * _isupper - checks if a character is upper case or not
- * @c: this is th echaracter to be checked
- * Return: the value to returned is 1 if c is upper case otherwise it is 0
+ * _isupper - check if a character is in upper case
+ * @c:- an int character
+ * Return: 1 for the uppercase letters and 0 for the lower ones
  */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
+	{
 		return (1);
+	}
 	else
-	        return (0);
+	{
+		return (0);
+	}
 }
