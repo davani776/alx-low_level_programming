385c46c0964008275dcadef0ed3acc973e3b0398
Dawit Kassahun <kassahundawit04@gmail.com>
2023-11-06 16:36:40 -0800

    an update

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - prints the file name
+ * Return: successfully ended
+ */
 int main(void)
 {
-	printf("%s\n",__FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
