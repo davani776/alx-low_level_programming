6a62f95f3fc80d4195d26f69d419228a70635f6b
root <kassahundawit04@gmail.com.com>
2024-02-26 00:17:52 -0800

    puts and printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - contains text
+ *
+ *Return:always 0
+ *i
+ *
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle/n");
+	return (0);
+}
